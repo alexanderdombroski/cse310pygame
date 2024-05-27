9b4ec3eb64f2e9dc16a3a1a01de82ce6a5f8735f
Scott May <scottmay612@gmail.com>
2024-05-27 10:23:52 -0600

    Added a green rectangle to the screen

--- run.py
+++ run.py
@@ -4,6 +4,10 @@ import pygame
 WIDTH = 800
 HEIGHT = 600
 
+# Save colors as variables for easy use.
+green = (34, 139, 34)
+white = (255,255,255)
+
 # Create the game window.
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
@@ -11,15 +15,26 @@ screen = pygame.display.set_mode((WIDTH, HEIGHT))
 clock = pygame.time.Clock()
 fps = 60
 
+# Set box dimensions.
+box_width = 20
+box_height = 40
+box_x = WIDTH // 2 - box_width // 2
+box_y = HEIGHT // 2 - box_height // 2
+box = pygame.Rect(box_x, box_y, box_width, box_height)
+
 running = True
 
 while running:
-    screen.fill((255,255,255))
+    # Fill the screen with the color white.
+    screen.fill(white)
 
     # Close the window if the user clicks the X button.
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+
+    # Draw the box on the screen.
+    pygame.draw.rect(screen, green, box)
 
     # Stores the amount of time since the last time tick was called.
     time_passed = clock.tick(fps) / 1000
