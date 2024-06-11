1a3e5fa609a5c36d08e9ff465213d6b93fb76cbb
ephraimjensen <jen22082@byui.edu>
2024-06-11 02:02:29 -0600

    reorganized so that updated code is in THE ACTUAL GAME

--- THE ACTUAL GAME/Wall2.py
+++ THE ACTUAL GAME/Wall2.py
@@ -7,18 +7,41 @@ class Wall(sprite.Sprite):
         color: Tuple[int, int, int], 
         left: int, 
         top: int, 
-        width: int, 
-        height: int, 
+        length: int,
+        is_horizontal: bool = True,
+        # width: int, 
+        # height: int, 
         groups: List[sprite.Group] = None
     ) -> None:
         
         # Add the sprites to groups
         super().__init__(*groups if groups else [])
 
-        self.image = Surface((width, height))
-        self.image.fill(Color(color))
+
+        tile_image = image.load("Prototypes/blank_room_example/wall_tile.png")
+        tile_width, tile_height = tile_image.get_size()
+
         
+        if not is_horizontal: #change this later
+            # Create the wall surface
+            self.image = Surface((tile_width, length))
+            # Tile the image on the wall surface
+            for i in range(length // tile_height):
+                self.image.blit(tile_image, (0, i * tile_height))
+        else:
+            # Create the wall surface
+            self.image = Surface((length, tile_width))
+            for i in range(length // tile_height):
+                self.image.blit(tile_image, (i * tile_height, 0))
+        
+        # Set the position
         self.rect = self.image.get_rect(topleft=(left, top))
+
+        # self.image = Surface((width, height))
+        # self.image.fill(Color(color))
+        
+        # self.rect = self.image.get_rect(topleft=(left, top))
+
     
     def vertically_aligned(self, x_left: int, x_right: int) -> bool:
         if x_left >= self.rect.left and x_left < self.rect.right:
@@ -49,3 +72,4 @@ class Wall(sprite.Sprite):
 
     def collide_bottom(self, y_top: int, y_bottom: int) -> bool:
         return y_bottom >= self.rect.top and y_top < self.rect.top
+    
