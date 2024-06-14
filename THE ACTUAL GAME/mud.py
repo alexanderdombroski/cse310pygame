e28dc42b50fb798373385c15d43c0566119db6b7
alexanderdombroski <dom21013@byui.edu>
2024-06-14 11:44:01 -0600

    added image for mud

--- THE ACTUAL GAME/mud.py
+++ THE ACTUAL GAME/mud.py
@@ -17,4 +17,13 @@ class Mud(sprite.Sprite):
         self.image = Surface((width, height))
         self.image.fill(Color((101, 67, 33)))
         
+        tile_image = image.load("images/mud.png")
+        tile_width, tile_height = tile_image.get_size()
+        
+        # Generate Image Tiles
+        self.image = Surface((width, height))
+        for x in range(0, width, tile_width):
+            for y in range(0, height, tile_height):
+                self.image.blit(tile_image, (x, y))
+
         self.rect = self.image.get_rect(topleft=(left, top))
