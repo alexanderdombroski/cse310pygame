b5cc25bc3d23777221bd244dfec19fb5dbf51edd
alexanderdombroski <dom21013@byui.edu>
2024-06-14 11:10:04 -0600

    added image for ice

--- THE ACTUAL GAME/ice.py
+++ THE ACTUAL GAME/ice.py
@@ -17,4 +17,13 @@ class Ice(sprite.Sprite):
         self.image = Surface((width, height))
         self.image.fill(Color((173, 216, 230)))
         
+        tile_image = image.load("images/ice.png")
+        tile_width, tile_height = tile_image.get_size()
+        
+        # Generate Image Tiles
+        self.image = Surface((width, height))
+        for x in range(0, width, tile_width):
+            for y in range(0, height, tile_height):
+                self.image.blit(tile_image, (x, y))
+
         self.rect = self.image.get_rect(topleft=(left, top))
